cbe56a2b2d0a85df5f6093217c4638b73238a664
Mohammad Faqusa <201014@ppu.edu.ps>
2024-04-13 01:35:24 +0300

    beginning to write listing lines

--- pass2.py
+++ pass2.py
@@ -3,6 +3,17 @@ from functions import read_json_file
 file_path = "data.json"
 intermediateList = read_json_file(file_path)
 
+listingLines = [] 
+
+
+firstLine = intermediateList[0] 
+opcode = firstLine['opcode']
+object_code = '' 
+listingLines.append(firstLine.update({}))
+if opcode == 'START' : 
+
+
+
 for i in range(len(intermediateList)) : 
 
     line = intermediateList[i] 
@@ -12,4 +23,5 @@ for i in range(len(intermediateList)) :
     operand = line['operand']
     locCount = int(line['LOCCTR'] , 16)
 
-    
+    if opcode == "START" : 
+
