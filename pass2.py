32e7120c8fba4ff068536616a4c2017251ca7aac
Mohammad Faqusa <201014@ppu.edu.ps>
2024-04-13 16:36:02 +0300

    complete writing listing lines program

--- pass2.py
+++ pass2.py
@@ -7,6 +7,8 @@ sic_directives = ["START", "END", "BYTE", "WORD", "RESB", "RESW", "BASE", "NOBAS
 intermediateList_path = "intermediate.json"
 symbols_path = "symbols.json" 
 
+opcodes = functions.file_to_dict("opcodeTable.txt")
+
 intermediateList = functions.read_json_file(intermediateList_path)
 symbols_dict = functions.read_json_file(symbols_path)
 
@@ -27,12 +29,13 @@ listingLines = []
 objectLines = [] 
 
 header = '' 
-firstLine = intermediateList[0] 
+firstLine = dict(intermediateList[0])
 opcode = firstLine['opcode']
 object_code = '' 
 
 if opcode == 'START' : 
-    listingLines.append(firstLine.update({'opjectCode' : object_code}))
+    firstLine.update({'opjectCode' : object_code})
+    listingLines.append(firstLine)
     
 header = "H^{0}^{1}^{2}".format(program_name, str(first_location_counter)[2:] , str(program_length)[2:])
 objectLines.append(header) 
@@ -45,21 +48,43 @@ for line in intermediateList[1:] :
     opcode = line['opcode']
     operand = line['operand']
     locCounter = line['LOCCTR']
-    operandValue = '' 
+    operandValue = ''
+    object_code = ''  
 
     if operand != '' and not operand.isdigit() and opcode not in sic_directives  : 
         # if operand in symbols_dict : 
         if operand in symbols_dict : 
             operandValue = symbols_dict[operand]
+            operandValue = str(operandValue)[2:]
 
         elif  operand.endswith(',X') :
             operandValue = symbols_dict[operand[:-2]]
+            operandValue = str(operandValue)[2:]
         
         else: 
             operandValue = 0 
             raise exceptions.UndefinedSymbol("undefine symbol operand  \"{}\" {} in line ({}) ".format(operand ,operandValue,  linCount))
-    print(linCount , opcode , operand , operandValue) 
+    else : 
+        operandValue = 0 
 
+    if opcode in sic_directives : 
+        if opcode == 'WORD' : 
+            operandValue = str(hex(int(operand , 16)))[2:]
+            object_code = operandValue.zfill(6) 
+        else : 
+            operandValue = '' 
+    else : 
+        object_code = (opcodes[opcode] + str(operandValue)).zfill(6)
+
+    listingline = dict(line)
+    listingline.update({'operand_value' : operandValue , 'object_code' : object_code})
+    listingLines.append(listingline) 
+
+    # listingLines.append(listingline)
+for x in listingLines : 
+    print(x) 
+# for x in listingLines : 
+#     print(x) 
 
 # for i in range(len(intermediateList)) : 
 
